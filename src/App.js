b1dbb2ec9ce664819515b7e2e75513d2eded2a8b
Santiago Sanchez Nieva <santiagosancheznieva@gmail.com>
2022-08-01 13:54:54 -0300

    Add: meme maker
    Now it's possible to put text on the image we choose.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [topText, setTopText] = useState("");
+  const [bottomText, setBottomText] = useState("");
+  const [image, setImage] = useState("");
+
+  const putTopText = (e) =>{
+    setTopText(e.target.value);
+  }
+  const putBottomText = (e) =>{
+    setBottomText(e.target.value);
+  }
+  const putImage = (e) => {
+    setImage(e.target.value);
+  }
+  
   return (
     <div className='App'>
-      <select>
+      <select onChange={putImage}>
         <option value="Disaster-Girl">Disaster girl</option>
-        <option value="Ancient-Aliens">Ancient aliens</option>
+        <option value="Ancients-Aliens">Ancients aliens</option>
         <option value="Futurama-Fry">Futurama</option>
         <option value="Duck-Meme">Duck</option>
-      </select>
+      </select> <br/>
+
+      <input onChange={putTopText} type="text" placeholder='Top text'/><br/>
+      <input onChange={putBottomText}type="text" placeholder='Bottom text'/><br/>
+
+      <div className='meme'>
+        <span className='meme__topText'>{topText}</span><br/>
+        <span className='meme__bottomText'>{bottomText}</span><br/>
+        <img src = {`/img/${image}.jpg`}  alt="Meme"/>
+      </div>
+
     </div>
   );
 }
