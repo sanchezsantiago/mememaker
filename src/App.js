2125487abd87e75a0a6546c3bf32577d21dc0dcc
Santiago Sanchez Nieva <santiagosancheznieva@gmail.com>
2022-08-01 12:52:52 -0300

    Add: Select meme & Images
    Select picker with the name of the image in the values of the options, so that it is easier to choose one.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import './App.css';
 
 function App() {
   return (
-    <>
-    
-    </>
+    <div className='App'>
+      <select>
+        <option value="Disaster-Girl">Disaster girl</option>
+        <option value="Ancient-Aliens">Ancient aliens</option>
+        <option value="Futurama-Fry">Futurama</option>
+        <option value="Duck-Meme">Duck</option>
+      </select>
+    </div>
   );
 }
+//Select image
+//Input text - First line
+//Input text - Second line
+//Export button
 
 export default App;
